c089ea770a8f61629150912f4d4a624f735ffdac
Edoardo <edoardoricca98@gmail.com>
2023-05-18 17:12:18 +0200

    ex9js added

---
+++ ex31/ex9.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+}
+  
+  // Print values of person using Object.keys
+
+let entries = Object.entries(person);       
+console.log(entries);
