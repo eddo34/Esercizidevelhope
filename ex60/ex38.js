1ad423e85fcc6c8fc4ac14aaa506fd84d664bca3
Edoardo <edoardoricca98@gmail.com>
2023-06-12 10:59:43 +0200

    ex38.js added

---
+++ ex60/ex38.js
@@ -0,0 +1,31 @@
+let isLogged = true;
+
+function logIn(param) {
+    return new Promise((resolve, reject) => { 
+    if(param) {
+        resolve(Math.random());
+    } else {
+        reject(new Error("Not Logged In!"))
+    }
+})
+};
+
+
+
+function secondPromise(number) {
+    return new Promise((resolve,reject) =>{
+    if(number > 0.5) {
+        resolve(`{name: "John", age: 24}`)
+    } else {
+        reject( new Error("Error!"))
+    }
+})
+};
+
+
+
+
+logIn(isLogged)
+    .then(secondPromise)
+    .then((val)=>console.log(val))
+    .catch((err)=>console.log(err))
