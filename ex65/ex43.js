f3659967ef0370a4f1035ffc2ab41963e7c271eb
Edoardo <edoardoricca98@gmail.com>
2023-06-15 16:21:18 +0200

    added ex43js

---
+++ ex65/ex43.js
@@ -0,0 +1,16 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+
+
+function localSaveObj(obj) {
+  let jsonObj = JSON.stringify(obj)
+  localStorage.setItem("user", `${jsonObj}` )
+}
+
+
+
+
