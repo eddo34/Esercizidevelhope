69b51c565eb01eac2abfba7f355bf67bca99e896
Edoardo <edoardoricca98@gmail.com>
2023-07-03 17:01:27 +0200

    ex2react added

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello.js';
 import Message from './Message.js';
+import Welcome from './Welcome.js';
 
 function App() {
   return (
     <div>
       <Hello/>
       <Message/>
+      <Welcome name='Eddo'/>
     </div>
   );
 }
