a4288767d0157a4d629f69746de99f8ef4b0cb79
Edoardo <edoardoricca98@gmail.com>
2023-09-04 18:31:35 +0200

    ex34 added

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,35 +1,15 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Hello from "./Hello.js";
 import Message from "./Message.js";
-import ClickCounter from "./ClickTracker";
-import InteractiveWelcome from "./InteractiveWelcome";
-import Login from "./Login";
-import { LanguageContext } from "./LanguageContext";
-import DisplayLanguage from "./DisplayLanguage";
-import { useState } from "react";
+import GithubUser from "./GithubUser";
 
 function App() {
-  const [language, setLanguage] = useState('En');
-
-  const handleChangeLanguage = (event) => {
-    setLanguage(event.target.value);
-  };
   return (
     <div>
       <Hello />
       <Message />
-      <div>
-        <select onChange={handleChangeLanguage} value={language}>
-          <option value="En">English</option>
-          <option value="It">Italian</option>
-          <option value="Jap">Japanese</option>
-        </select>
-        <LanguageContext.Provider value={language}>
-          <DisplayLanguage />
-        </LanguageContext.Provider>
-      </div>
-
+      <GithubUser username="eddo34"/>
     </div>
   );
 }
