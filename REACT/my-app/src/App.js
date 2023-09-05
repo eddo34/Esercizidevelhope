0d37fde1356ce64606baea2c371a6fa551d5e290
Edoardo <edoardoricca98@gmail.com>
2023-09-05 17:25:50 +0200

    ex-46 added

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Counter from "./Counter"; 
 import ShowGithubUser from "./ShowGithubUser";
 
 function App() {
   return (
+    <>
+      <Link to="/counter">Counter </Link>
+      <Link to="/users/eddo34"> Users</Link>
       <Routes>
         <Route path="/counter" element={<Counter /> } />
         <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
+    </>
   );
 }
 
