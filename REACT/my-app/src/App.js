8e23fe383db7fdcb792989fec99ba52ac9045307
Edoardo <edoardoricca98@gmail.com>
2023-09-05 17:12:02 +0200

    ex45 added

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Counter from "./Counter"; 
+import ShowGithubUser from "./ShowGithubUser";
 
 function App() {
   return (
       <Routes>
         <Route path="/counter" element={<Counter /> } />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
   );
 }
